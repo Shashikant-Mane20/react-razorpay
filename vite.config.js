f69e07d81da685c1caa55103df45600e08b346f8
[shashikant] <[shashikant.mane801@gmail.com]>
2024-10-28 17:58:06 +0530

    Initial Commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig ,loadEnv} from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  }
+})
+
